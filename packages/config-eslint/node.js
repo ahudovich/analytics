709a2b9afd44a6450421b05d78e6d2cb10d5a9bd
Andrei Hudovich <andrei@hudovich.com>
2025-11-02 21:41:33 +0100

    chore: simplify eslint config

--- packages/config-eslint/node.js
+++ packages/config-eslint/node.js
@@ -1,14 +1,4 @@
-import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import { baseConfig } from './index.js'
 
-export const nodeConfig = defineConfig([
-  ...baseConfig,
-  {
-    name: '@repo/config-eslint/node',
-    files: ['**/*.ts'],
-    languageOptions: {
-      globals: globals.node,
-    },
-  },
-])
+export const nodeConfig = defineConfig([...baseConfig])
--- packages/config-eslint/vite.js
+++ packages/config-eslint/vite.js
@@ -1,14 +1,4 @@
-import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import { baseConfig } from './index.js'
 
-export const viteConfig = defineConfig([
-  ...baseConfig,
-  {
-    name: '@repo/config-eslint/vite',
-    files: ['**/*.ts'],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-])
+export const viteConfig = defineConfig([...baseConfig])
