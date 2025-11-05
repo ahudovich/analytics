ace338c102bdaad639dcceee5a882b89acf0fb8d
Andrei Hudovich <andrei@hudovich.com>
2025-11-05 13:31:20 +0100

    chore: add enums package

--- apps/tracker/src/core.ts
+++ apps/tracker/src/core.ts
@@ -1,23 +1,30 @@
+import { Event } from '@repo/enums/events'
+
 function init() {
   document.addEventListener('DOMContentLoaded', () => {
-    sendEvent({
+    sendEvent(Event.PageView, {
       screen: {
         width: screen.width,
         height: screen.height,
       },
-      language: navigator.language,
     })
   })
 }
 
-async function sendEvent(data: Record<string, unknown>) {
+async function sendEvent(event: Event, data: Record<string, unknown>) {
   try {
+    const payload = {
+      event,
+      createdAt: new Date().toISOString(),
+      ...data,
+    }
+
     await fetch('#', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(payload),
       keepalive: true,
     })
   } catch (error: unknown) {
---
+++ packages/enums/src/events.ts
@@ -0,0 +1,3 @@
+export enum Event {
+  PageView = 'page_view',
+}
