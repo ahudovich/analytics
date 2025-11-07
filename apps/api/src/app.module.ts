3c52431f4d61ead9d8ada434d3faa02128cde1c3
Andrei Hudovich <andrei@hudovich.com>
2025-11-07 17:42:27 +0100

    refactor(api): remove unused app controller and service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller.js'
-import { AppService } from './app.service.js'
 import { IngestModule } from './ingest/ingest.module.js'
 import { HealthModule } from './health/health.module.js'
 
 @Module({
   imports: [IngestModule, HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
