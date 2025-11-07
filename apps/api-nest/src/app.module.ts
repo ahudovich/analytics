e3d3f08143598c91ce064b20608bcd7f5e2ccb21
Andrei Hudovich <andrei@hudovich.com>
2025-11-07 17:29:31 +0100

    feat(api): add health module

--- apps/api-nest/src/app.module.ts
+++ apps/api-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller.js'
 import { AppService } from './app.service.js'
 import { IngestModule } from './ingest/ingest.module.js'
+import { HealthModule } from './health/health.module.js'
 
 @Module({
-  imports: [IngestModule],
+  imports: [IngestModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
